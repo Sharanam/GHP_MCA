ed70d26178697fc08c144c4a675ef1c068929b03
38852691 <38852691+Sharanam@users.noreply.github.com>
2021-12-13 20:46:11 +0530

    schedule

---
+++ src/lib/sem1/schedule/midterm.svelte
@@ -0,0 +1,93 @@
+<script>
+    const schedule = [
+        {
+            date: "08/01/2022",
+            subject: {
+                code: "51",
+                name: "Python Programming",
+            },
+        },
+        {
+            date: "10/01/2022",
+            subject: {
+                code: "52",
+                name: "Computer Networks",
+            },
+        },
+        {
+            date: "11/01/2022",
+            subject: {
+                code: "53",
+                name: "DBMS",
+            },
+        },
+        {
+            date: "12/01/2022",
+            subject: {
+                code: "54",
+                name: "Operating Systems",
+            },
+        },
+        {
+            date: "13/01/2022",
+            subject: {
+                code: "55",
+                name: "Computer Fundamentals",
+            },
+        },
+        {
+            date: "18/01/2022",
+            subject: {
+                code: "56",
+                name: "DBMS & OS",
+            },
+        },
+        {
+            date: "19/01/2022",
+            subject: {
+                code: "57",
+                name: "Python Programming",
+            },
+        },
+    ];
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th colspan="3">Internal Test Schedule</th>
+        </tr>
+        <tr>
+            <th> Date </th>
+            <th> Subject (Code & Name) </th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each schedule as subject}
+            <tr>
+                <td>{subject.date}</td>
+                <td>PS01CMCA{subject.subject.code} : {subject.subject.name}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style>
+    table,
+    tr,
+    td,
+    th {
+        border: 1px solid white;
+        border-collapse: collapse;
+        padding: 7px;
+        text-align: center;
+        width: max-content;
+        margin: auto;
+    }
+    tr > td {
+        text-align: left;
+    }
+    table {
+        border: 1px solid wheat;
+    }
+</style>
