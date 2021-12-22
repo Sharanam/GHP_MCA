c1083f4634a103ca73fd47c838d6276a08b5e7f2
Sharanam Maheshbhai Chotai <38852691+Sharanam@users.noreply.github.com>
2021-12-22 15:55:35 +0530

    App update (#11)
    * manifest file
    
    * navbar key binding
    
    * npm script host add
    
    * schedule
    
    * CF DBC doc upload
    
    * python internals syllabus declared

---
+++ src/lib/sem1/subjects/python/Syllabus.svelte
@@ -0,0 +1,49 @@
+<div class="innerSection">
+    <h2 class="mb">Syllabus</h2>
+    <ol>
+        <li>
+            Introduction to Python
+            <ol>
+                <li>Brief history, key characteristics,</li>
+                <li>Advantages & Python Application Areas</li>
+                <li>Syntax overview, comments, naming conventions</li>
+                <li>Primitive data types, data type constructors</li>
+                <li>Operators</li>
+                <li>Console input and output</li>
+                <li>Control structures</li>
+            </ol>
+        </li>
+        <li>
+            Aggregate Data Types
+            <ol>
+                <li>Sequence types: lists, tuple, range</li>
+                <li>Common operations on sequences</li>
+                <li>Operations on lists</li>
+                <li>Using tuples and ranges</li>
+                <li>Text sequence type: str</li>
+                <li>Operations on strings</li>
+            </ol>
+        </li>
+        <li>
+            Composite Data Types, Functions and Exception Handling
+            <ol>
+                <li>Mapping type : dict</li>
+                <li>Set type: set</li>
+                <hr />
+                <li>Functions</li>
+                <li>Exception handling</li>
+            </ol>
+        </li>
+        <li>
+            Additional Features of Python
+            <ol>
+                <li>Object-oriented programming in Python</li>
+                <li>File handling in Python</li>
+                <li>Modules and packages</li>
+                <li>
+                    Introduction to GUI applications and database connectivity
+                </li>
+            </ol>
+        </li>
+    </ol>
+</div>
