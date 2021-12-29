175f5f09f98bbb7db30c38b573fd9a5e4c07528f
38852691 <38852691+Sharanam@users.noreply.github.com>
2021-12-29 13:22:15 +0530

    CN Internals

---
+++ public/Materials/Devang Pastagia/Computer Networks/README.md
@@ -0,0 +1,2 @@
+# Computer Networks (Internal Exams)
+- PDF file in this directory was shared by Dr. DBC to the Self-Financed-Batc Students. Since the syllabus and question paper will be identical for both the batches, students should consider it as single source of truth.
--- src/lib/sem1/subjects/cn/Syllabus.svelte
+++ src/lib/sem1/subjects/cn/Syllabus.svelte
@@ -1,41 +1,90 @@
-<section class="my">
-    <h1>1. Introduction and Data Communication Fundamentals</h1>
-<ul>
-<li>Introduction to computer networks</li>
-<li>Classification of computer networks</li>
-<li>Transmission media : guided and unguided media.</li>
-<li>Functions of network connecting devices: Amplifier, Repeater, Bridge, Hub, Switch, Router, Gateway, Modems</li>
-<li>Data transmission concepts : transmission modes, multiplexing, switching technologies, asynchronous and synchronous transmission</li>
-<li>Introduction to Local Area Networks (LANs), LAN topologies,</li>
-<li>Gigabit Ethernet</li>
-</ul>
-<h1>2. Layered Protocols</h1>
-<ul>
-<li>Protocols, Protocol hierarchies</li>
-<li>Design issues for the layers</li>
-<li>The OSI reference model and the TCP/IP reference model</li>
-<li>The Internet Protocol (IP), IP addresses, Subnets,</li>
-<li>Introduction to Transmission Control Protocol (TCP), The TCP segment header</li>
-<li>Introduction to User Datagram Protocol (UDP)</li>
-</ul>
-<h1>3. Routing, Congestion Control and Internetworking</h1>
-<ul>
-<li>Virtual Circuits and Datagrams</li>
-<li>Routing Techniques</li>``
-<li>Congestion Control</li>
-<li>Introduction and Issues that arise in Internetworking</li>
-<li>Fragmentation</li>
-<li>Tunneling</li>
-<li>Virtual Private Networks</li>
-</ul>
-<h1>4. Wireless Communication and Network Security</h1>
-<ul>
-<li>Introduction to Wireless Networks</li>
-<li>Satellite Communication (LEO, MEO, GEO)</li>
-<li>Wireless LAN protocols</li>
-<li>Introduction to Mobile Telephone Systems, Cell Fundamentals</li>
-<li>Traditional Cryptography, Substitution Cipher vs Transposition Cipher, Fundamental Cryptographic Principles</li>
-<li>Secret-Key Algorithms vs Public-Key Algorithms</li>
-<li>Firewalls</li>
-</ul>
-</section>
+<div class="innerSection">
+    <div class="innerSection">
+        <q title="In Official Whatsapp-Group">
+            Syllabus for Internal examination for PS01CMCA52 - Computer Networks
+            is
+            <span class="bold">
+                First two units plus introduction and issues that arise in
+                Internetworking and Routing techniques topics from unit 3.
+            </span>
+        </q>
+        : Dr. PVV
+    </div>
+    <div class="innerSection center">
+        <a
+            href="https://github.com/Sharanam/GHP_MCA/raw/master/public/Materials/Devang%20Pastagia/Computer%20Networks/MCA-I%20Computer%20Networks%20Internals.pdf"
+            title="Notes"
+            target="_blank"
+        >
+            Click here to download Internals' Notes !!!
+        </a>
+    </div>
+    <h1>Syllabus</h1>
+    <h3>1. Introduction and Data Communication Fundamentals</h3>
+    <ul>
+        <li>Introduction to computer networks</li>
+        <li>Classification of computer networks</li>
+        <li>Transmission media : guided and unguided media.</li>
+        <li>
+            Functions of network connecting devices: Amplifier, Repeater,
+            Bridge, Hub, Switch, Router, Gateway, Modems
+        </li>
+        <li>
+            Data transmission concepts : transmission modes, multiplexing,
+            switching technologies, asynchronous and synchronous transmission
+        </li>
+        <li>Introduction to Local Area Networks (LANs), LAN topologies,</li>
+        <li>Gigabit Ethernet</li>
+    </ul>
+    <h3>2. Layered Protocols</h3>
+    <ul>
+        <li>Protocols, Protocol hierarchies</li>
+        <li>Design issues for the layers</li>
+        <li>The OSI reference model and the TCP/IP reference model</li>
+        <li>The Internet Protocol (IP), IP addresses, Subnets,</li>
+        <li>
+            Introduction to Transmission Control Protocol (TCP), The TCP segment
+            header
+        </li>
+        <li>Introduction to User Datagram Protocol (UDP)</li>
+    </ul>
+    <h3>3. Routing, Congestion Control and Internetworking</h3>
+    <ul>
+        <li>Virtual Circuits and Datagrams</li>
+        <li>Routing Techniques</li>
+        <li>Congestion Control</li>
+        <li>Introduction and Issues that arise in Internetworking</li>
+        <li>Fragmentation</li>
+        <li>Tunneling</li>
+        <li>Virtual Private Networks</li>
+    </ul>
+    <h3>4. Wireless Communication and Network Security</h3>
+    <ul>
+        <li>Introduction to Wireless Networks</li>
+        <li>Satellite Communication (LEO, MEO, GEO)</li>
+        <li>Wireless LAN protocols</li>
+        <li>Introduction to Mobile Telephone Systems, Cell Fundamentals</li>
+        <li>
+            Traditional Cryptography, Substitution Cipher vs Transposition
+            Cipher, Fundamental Cryptographic Principles
+        </li>
+        <li>Secret-Key Algorithms vs Public-Key Algorithms</li>
+        <li>Firewalls</li>
+    </ul>
+</div>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    h3 {
+        text-decoration: underline;
+    }
+    .bold {
+        font-weight: bold;
+    }
+    a {
+        text-decoration: none;
+        border-bottom: 1px solid #ccc;
+    }
+</style>
